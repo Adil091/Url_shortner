50b1f546f067066dfc57bba32f5b40bf3d84e29f
adil <adilmulla091@gmail.com>
2025-05-03 22:21:49 +0530

    created colums to save the information in django

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,8 +3,13 @@ from django.db import models
 
 # Create your models here.
 class ShortzyURL(models.Model):
-    url = models.CharField(max_length=220)
-    shortcode = models.CharField(max_length=15,unique=True,null=True)
+    url        =  models.CharField(max_length=220)
+    shortcode  =  models.CharField(max_length=15,unique=True,null=True)
+    updated    =  models.DateTimeField(auto_now=True)
+    timestamp  =  models.DateTimeField(auto_now_add=True)
+    #timestamp = models.DateTimeField(auto_now=false,auto_now_add=False)
+
+
     def __str__(self):
         return str(self.url)
     def __unicode__(self):
