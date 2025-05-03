6ecbd5c75263e9276ec824a4a74b48056ea84bca
adil <adilmulla091@gmail.com>
2025-05-03 22:09:26 +0530

    setup a django server

---
+++ src/shortener/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+# Create your models here.
+class ShortzyURL(models.Model):
+    url = models.CharField(max_length=220)
+    shortcode = models.CharField(max_length=15,unique=True,null=True)
+    def __str__(self):
+        return str(self.url)
+    def __unicode__(self):
+        return str(self.url)
