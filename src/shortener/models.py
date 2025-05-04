0ce179126d37721ca08e2014cd13008879069faa
adil <adilmulla091@gmail.com>
2025-05-04 19:39:00 +0530

    created custom app and random string genrator on save

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -1,13 +1,22 @@
+import random
 from django.db import models
-
+from django.utils import timezone
 
 # Create your models here.
+def code_generator(size=6, chars='abcdefghijklmnopqrstuvwxyz'):
+    return ''.join(random.choice(chars) for _ in range(size))
 class ShortzyURL(models.Model):
     url        =  models.CharField(max_length=220)
     shortcode  =  models.CharField(max_length=15,unique=True,null=True)
     updated    =  models.DateTimeField(auto_now=True)
-    timestamp  =  models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(default=timezone.now)
     #timestamp = models.DateTimeField(auto_now=false,auto_now_add=False)
+
+    def save(self,*args,**kwargs):
+        print("something")
+        self.shortcode = code_generator()
+        super(ShortzyURL,self).save(*args,**kwargs)
+
 
 
     def __str__(self):
